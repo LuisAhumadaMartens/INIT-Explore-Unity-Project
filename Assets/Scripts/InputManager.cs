33ee0d072ec1062df5133b9430dc9108e44828d8
Luis Ahumada <luis@ahumada.dev>
2023-09-28 13:53:20 -0400

    Workshop

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -28,29 +28,25 @@ public class InputManager : MonoBehaviour
     private void Awake()
     {
         //Set controls to FPS asset
-        controls = new FPS();
+        
 
         //Set the input action maps accordingly
-        groundMovement = controls.Movement;
-        cameraLook = controls.Camera;
+        
 
         //Map movement input vector to vector updated in the action map
-        groundMovement.PlayerMove.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
+        
 
         //Map x and y values of the mouse based on input map
-        cameraLook.MouseX.performed += ctx => mouseX = ctx.ReadValue<float>();
-        cameraLook.MouseY.performed += ctx => mouseY = ctx.ReadValue<float>();
+        
 
         //Map Jump input to movement
-        groundMovement.Jump.performed += ctx => jump = ctx.ReadValueAsButton();
+        
     }
 
     private void Update()
     {
         //Send input values to necessary scripts
-        playerMove.RecieveInput(movementInput);
-        playerMove.RecieveJump(jump);
-        mouseLook.RecieveInput(mouseX, mouseY);
+        
     }
 
     //Enable and disable input asset
